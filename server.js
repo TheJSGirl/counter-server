18ef52ab332bcc33cecd7e3c7f92330f79ba44ad
TheJSGirl <prinegi100@gmail.com>
2019-03-30 22:41:07 +0530

    feat: modified server.js

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const io = require('socket.io')(9000);
+const redis = require('redis');
+const client = redis.createClient({port:6379, host:"0.0.0.0"});
 
 io.on('connection', socket => {
-    socket.on('hello', hashID => console.log(hashID))
+    socket.on('hello', async(data) => {
+        const count = await inc(data);
+        console.log(count)
+
+    })
 })
+
+
+async function inc(data){
+    return new Promise(resolve => {
+        client.incr(data.hashID, (err, count) => {
+            resolve(count)
+        })
+    });
+}
