9e11549aeb32e77950f3555bf431c555764a5423
TheJSGirl <prinegi100@gmail.com>
2019-04-11 20:09:32 +0530

    emit event for hit counts

--- server.js
+++ server.js
@@ -4,17 +4,8 @@ const client = redis.createClient({port:6379, host:"0.0.0.0"});
 
 io.on('connection', socket => {
     socket.on('hello', async(data) => {
-        const count = await inc(data);
-        console.log(count)
-
-    })
-})
-
-
-async function inc(data){
-    return new Promise(resolve => {
         client.incr(data.hashID, (err, count) => {
-            resolve(count)
+            socket.emit('stats', count)
         })
-    });
-}
+    })
+});
